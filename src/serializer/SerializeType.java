0d37c7c09e374b66d645261bafb736c5e47635c0
laiwenqiang <1260091093@qq.com>
2017-10-29 23:53:09 +0800

    DefaultJavaSerializer

--- src/serializer/SerializeType.java
+++ src/serializer/SerializeType.java
@@ -1,22 +1,36 @@
 package serializer;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by laiwenqiang on 2017/10/29.
  */
 public enum SerializeType {
 
     DefaultJavaSerializer("DefaultJavaSerializer"),
-    HessianSerializer("HessianSerializer"),
-    JSONSerializer("JSONSerializer"),
-    XmlSerializer("XmlSerializer"),
-    AvroSerializer("AvroSerializer"),
-    ProtocoBufferSerializer("ProtocoBufferSerializer"),
-    ThriftSerializer("ThriftSerializer");
+    HessianSerializer("HessianSerializer");
 
     private String serializeType;
 
-    private SerializeType(String serializeType) {
+    SerializeType(String serializeType) {
         this.serializeType = serializeType;
     }
+
+    public static SerializeType quaryByType(String serializeType) {
+        if (StringUtils.isBlank(serializeType)) {
+            return null;
+        }
+        for (SerializeType serialize : SerializeType.values()) {
+            if (StringUtils.equals(serialize.getSerializeType(), serializeType)) {
+               return serialize;
+            }
+        }
+        return null;
+    }
+
+    public String getSerializeType() {
+        return serializeType;
+    }
+
 
 }
--- src/serializer/SerializerEngine.java
+++ src/serializer/SerializerEngine.java
@@ -6,7 +6,19 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * Created by laiwenqiang on 2017/10/29.
  */
-public class SerializerEngine {
+public class SerializerEngine implements ISerializer{
     public static final Map<SerializeType, ISerializer> serializerMap = new ConcurrentHashMap<SerializeType, ISerializer>();
 
+    static {
+        serializerMap.put(SerializeType.DefaultJavaSerializer, new DefaultJavaSerializer());
+        serializerMap.put(SerializeType.HessianSerializer, new HessianSerializer());
+    }
+
+    public <T> byte[] serialize(T obj) {
+        return new byte[0];
+    }
+
+    public <T> T deserialize(byte[] data, Class<T> clazz) {
+        return null;
+    }
 }
